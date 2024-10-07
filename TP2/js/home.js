70e64ae70379279875a36fcf2cbf78b4b7dfdcf4
romanmeclazcke1@gmail.com <romanmeclazcke1@gmail.com>
2024-10-07 10:14:05 -0300

    agregue pantalla de carga y solucione errores del movimiento del carrusel

--- TP2/js/home.js
+++ TP2/js/home.js
@@ -1,6 +1,11 @@
+document.getElementById("loader").style.display = "flex";
+let header =document.querySelector  (".header")
 let menu = document.getElementById("menu-hamburguesa");
 let btnCerrarMenu = document.getElementById("btn-cerrar-menu");
 let btnAbrirMenu = document.getElementById("btn-abrir-menu");
+
+menu.style.display = "none";
+header.style.display="none";
 
 btnAbrirMenu.addEventListener("click", (e) => {
     e.preventDefault();
@@ -12,33 +17,50 @@ btnCerrarMenu.addEventListener("click", (e) => {
     menu.style.display = "none";
 });
 
-let carruseles = document.querySelectorAll(".carrusel");
 
+let progress = 0;
+let barra = document.querySelector('.barra-carga');
+let porcentajeCarga = document.getElementById('porcentaje-carga');
+
+let interval = setInterval(() => {
+    progress += 10;
+    barra.style.width = progress + '%';
+    porcentajeCarga.textContent = progress + '%';
+
+    if (progress >= 100) {
+        header.style.display="flex"
+        document.getElementById("loader").style.display = "none";
+        clearInterval(interval); //evito que siga sumando las variable
+    }
+}, 500);
+
+// Carruseles normales
+let carruseles = document.querySelectorAll(".carrusel");
 carruseles.forEach((carrusel) => {
     let btnIzq = carrusel.querySelector(".btn-izq");
     let btnDer = carrusel.querySelector(".btn-der");
     let containerCards = carrusel.querySelector(".container-cards-carrusel");
     let cardsNormal = carrusel.querySelectorAll(".container-card");
-    let rectanguloIzq = carrusel.querySelector(".rectangulo-izq")
-    let rectanguloDer = carrusel.querySelector(".rectangulo-der")
+    let rectanguloIzq = carrusel.querySelector(".rectangulo-izq");
+    let rectanguloDer = carrusel.querySelector(".rectangulo-der");
     let movimientoEnPixeles = 600;
-    let totalPixels = cardsNormal.length * 300; //tamanio de la card
-    let movimientosActualesEnPixeles = containerCards.clientWidth; //siempre arranco desde el ancho del contendor dado que sino funciona mal(tomo el ancho del contenedor a desplazar)
+    let totalPixels = cardsNormal.length * 300;
+    let movimientosActualesEnPixeles = containerCards.clientWidth;
 
     btnIzq.addEventListener("click", () => {
         movimientosActualesEnPixeles -= movimientoEnPixeles;
 
         containerCards.scrollBy({
             left: -movimientoEnPixeles,
-            behavior: 'smooth' // Scroll suave
+            behavior: 'smooth'
         });
 
         if (rectanguloDer.style.display == "none") {
-            rectanguloDer.style.display = "flex"
+            rectanguloDer.style.display = "flex";
         }
         if (movimientosActualesEnPixeles <= containerCards.clientWidth) {
             movimientosActualesEnPixeles = containerCards.clientWidth;
-            rectanguloIzq.style.display = "none"
+            rectanguloIzq.style.display = "none";
         }
 
         console.log("Total pixeles: " + totalPixels);
@@ -50,23 +72,23 @@ carruseles.forEach((carrusel) => {
 
         containerCards.scrollBy({
             left: movimientoEnPixeles,
-            behavior: 'smooth' // Scroll suave
+            behavior: 'smooth'
         });
 
         if (rectanguloIzq.style.display == "none") {
-            rectanguloIzq.style.display = "flex"
+            rectanguloIzq.style.display = "flex";
         }
         if (movimientosActualesEnPixeles >= totalPixels) {
             movimientosActualesEnPixeles = totalPixels;
-            rectanguloDer.style.display = "none"
+            rectanguloDer.style.display = "none";
         }
+
         console.log("Total pixeles: " + totalPixels);
         console.log("Movimiento actual: " + movimientosActualesEnPixeles);
     });
 });
 
-/*Movimiento carrusel especial */
-
+// Carrusel especial
 let carruselEspecial = document.querySelectorAll(".carrusel-especial");
 
 carruselEspecial.forEach((carrusel) => {
@@ -74,32 +96,31 @@ carruselEspecial.forEach((carrusel) => {
     let btnDerEspecial = carrusel.querySelector(".btn-der");
     let containerCardsEspecial = carrusel.querySelector(".container-cards-carrusel");
     let cardsEspeciales = carrusel.querySelectorAll(".container-card-especial");
-    let rectanguloIzqEspecial = carrusel.querySelector(".rectangulo-izq")
-    let rectanguloDerEspecial = carrusel.querySelector(".rectangulo-der")
+    let rectanguloIzqEspecial = carrusel.querySelector(".rectangulo-izq");
+    let rectanguloDerEspecial = carrusel.querySelector(".rectangulo-der");
     let movimientoEnPixelesEspecial = 600;
-    let totalPixelsEspecial = cardsEspeciales.length * 430; //tamanio de la card especial
-    let movimientosActualesEnPixelesEspecial = containerCardsEspecial.clientWidth; //siempre arranco desde el ancho del contendor dado que sino funciona mal(tomo el ancho del contenedor a desplazar)
-    console.log("total  pixeles  "+totalPixelsEspecial )
+    let totalPixelsEspecial = cardsEspeciales.length * 430;
+    let movimientosActualesEnPixelesEspecial = containerCardsEspecial.clientWidth;
 
     btnIzqEspecial.addEventListener("click", () => {
         movimientosActualesEnPixelesEspecial -= movimientoEnPixelesEspecial;
 
         containerCardsEspecial.scrollBy({
             left: -movimientoEnPixelesEspecial,
-            behavior: 'smooth' // Scroll suave
+            behavior: 'smooth'
         });
 
         if (rectanguloDerEspecial.style.display == "none") {
-            rectanguloDerEspecial.style.display = "flex"
+            rectanguloDerEspecial.style.display = "flex";
         }
         if (movimientosActualesEnPixelesEspecial <= containerCardsEspecial.clientWidth) {
             movimientosActualesEnPixelesEspecial = containerCardsEspecial.clientWidth;
-            rectanguloIzqEspecial.style.display = "none"
+            rectanguloIzqEspecial.style.display = "none";
         }
 
         console.log("toque izq Total pixeles: " + totalPixelsEspecial);
         console.log("Movimiento actual: " + movimientosActualesEnPixelesEspecial);
-        rotarDerecha(cardsEspeciales)
+        rotarDerecha(cardsEspeciales);
     });
 
     btnDerEspecial.addEventListener("click", () => {
@@ -107,19 +128,20 @@ carruselEspecial.forEach((carrusel) => {
 
         containerCardsEspecial.scrollBy({
             left: movimientoEnPixelesEspecial,
-            behavior: 'smooth' // Scroll suave
+            behavior: 'smooth'
         });
 
         if (rectanguloIzqEspecial.style.display == "none") {
-            rectanguloIzqEspecial.style.display = "flex"
+            rectanguloIzqEspecial.style.display = "flex";
         }
         if (movimientosActualesEnPixelesEspecial >= totalPixelsEspecial) {
             movimientosActualesEnPixelesEspecial = totalPixelsEspecial;
-            rectanguloDerEspecial.style.display = "none"
+            rectanguloDerEspecial.style.display = "none";
         }
+
         console.log("Total pixeles: " + totalPixelsEspecial);
         console.log("Movimiento actual: " + movimientosActualesEnPixelesEspecial);
-        rotarIzquierda(cardsEspeciales)
+        rotarIzquierda(cardsEspeciales);
     });
 });
 
