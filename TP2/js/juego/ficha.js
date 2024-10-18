5d1d90d88ba5faec3ea30d45f5f84d57dcda7247
romanmeclazcke1@gmail.com <romanmeclazcke1@gmail.com>
2024-10-18 10:18:35 -0300

    cree clase ficha y agregue atributos

--- TP2/js/juego/ficha.js
+++ TP2/js/juego/ficha.js
@@ -1,3 +1,16 @@
 class Ficha{
+    constructor(ctx,color,jugador,x,y){
+        this.ctx=ctx
+        this.color=color
+        this.jugador=jugador
+        this.x=x
+        this.y=y
+    }
+
+
+
+    estaSeleccionada(posicionXMouse, posicionYMouse){
+        
+    }
 
 }
