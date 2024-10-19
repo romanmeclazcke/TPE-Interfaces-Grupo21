4f90d5b7c06c1cd4261223491d19abf032b5d00f
romanmeclazcke1@gmail.com <romanmeclazcke1@gmail.com>
2024-10-19 12:40:52 -0300

    cree logica para verificar si una ficha esta seleccionada y dibujarla

--- TP2/js/juego/ficha.js
+++ TP2/js/juego/ficha.js
@@ -1,16 +1,35 @@
-class Ficha{
-    constructor(ctx,color,jugador,x,y){
-        this.ctx=ctx
-        this.color=color
-        this.jugador=jugador
+class Ficha {
+
+    constructor(ctx, color, jugador, x, y, radio,imgSrc) {
+        this.ctx = ctx;
+        this.color = color;
+        this.jugador = jugador;
+        this.x = x;
+        this.y = y;
+        this.radio = radio; 
+        this.imgSrc = imgSrc;
+    }
+
+    draw() {
+        this.ctx.beginPath();
+        this.ctx.arc(this.x, this.y, this.radio, 0, 2 * Math.PI); 
+        this.ctx.fillStyle = this.color;
+        this.ctx.fill();
+        this.ctx.stroke();
+        this.ctx.closePath();
+    }
+
+
+    estaSeleccionada(posicionXMouse, posicionYMouse) {
+        let _x = this.x-posicionXMouse
+        let _y = this.y-posicionYMouse
+        return Math.sqrt(_x*_x*+_y*_y)<this.radio
+    }
+
+    setearPosicion(x,y){
         this.x=x
         this.y=y
     }
 
-
-
-    estaSeleccionada(posicionXMouse, posicionYMouse){
-        
-    }
-
+    
 }
