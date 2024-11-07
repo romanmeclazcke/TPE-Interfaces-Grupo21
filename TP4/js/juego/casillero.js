9e4aec9e3680d28063afb8f4f9f7fab0cc11a420
romanmeclazcke1@gmail.com <romanmeclazcke1@gmail.com>
2024-11-07 15:16:52 -0300

    comienzo tpe4

---
+++ TP4/js/juego/casillero.js
@@ -0,0 +1,47 @@
+class Casillero {
+    constructor(ctx, imgSrc,x,y,tamanio) {
+        this.ctx = ctx;
+        this.img = new Image();
+        this.img.src = imgSrc;
+        this.x=x
+        this.y=y
+        this.fichaSeteada=null
+        this.tamanioCelda=tamanio;
+        this.imagenCargada = new Promise((resolve) => {
+            this.img.onload = () => {
+                resolve(); // Resuelvo la promesa cuando se termina de cargar la imagen
+            };
+        });
+    }
+
+    estaLibre() {
+        return this.fichaSeteada===null;
+    }
+
+    setearFicha(ficha){
+        this.fichaSeteada=ficha;
+    }
+
+    async draw() {
+        await this.imagenCargada;
+        this.ctx.drawImage(this.img, this.x, this.y, this.tamanioCelda, this.tamanioCelda); 
+    }
+    
+    drawFondo(){
+        this.ctx.fillStyle='rgba(209, 209, 209, 0.8)'
+        this.ctx.fillRect(this.x,this.y,this.tamanioCelda,this.tamanioCelda)//color del fondo de la casilla
+    }
+    getPosicion(){
+        return {
+            x:this.x,y:this.y
+        }
+    }
+
+    getTamanio(){
+        return this.tamanioCelda
+    }
+
+    getFicha(){
+        return this.fichaSeteada;
+    }
+}
