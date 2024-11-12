c637256d1cf402e0b11fae179a0ac5a707c85e43
romanmeclazcke1@gmail.com <romanmeclazcke1@gmail.com>
2024-11-12 10:35:16 -0300

    agregue movimientos en los elementos

--- TP4/js/pixelperfect.js
+++ TP4/js/pixelperfect.js
@@ -9,3 +9,45 @@ btnMenu.addEventListener('click', () => {
   lin3.classList.add('active');
   lin2.style.visibility = 'hidden'; 
 });
+
+
+document.addEventListener('scroll', function() {
+  const scrollTop = window.scrollY; //cantidad de pixeles que se scrollearon
+  let logo = document.getElementById('logo')
+
+  const elements = [
+    { selector: '.container-pasto-1', speed: 0.1 },
+    { selector: '.container-arbol-3', speed: 0.2 },
+    { selector: '.container-pasto-2', speed: 0.3 },
+    { selector: '.container-arbol-2', speed: 0.4 },
+    { selector: '.container-piedra-3', speed: 0.5 },
+    { selector: '.container-piedra-1', speed: 0.6 },
+    { selector: '.container-piedra-4', speed: 0.7 },
+    { selector: '.container-personaje-3', speed: 0.80 },
+    { selector: '.container-personaje-2', speed: 0.80 },
+    { selector: '.container-personaje-1', speed: 0.80 },
+    { selector: '.container-pasto-4', speed: 0.92 },
+    { selector: '.container-arbol-1', speed: 0.93 },
+    { selector: '.container-piedra-2', speed: 0.94 },
+    { selector: '.container-pasto-3', speed: 0.95 },
+    { selector: '.container-logo', speed: 0.99 }
+  ];
+
+  if (scrollTop > 150) {
+    logo.style.height = "86px"
+    logo.style.width = "150px"
+    logo.style.left="45%"
+    logo.style.top="36px";
+  } else {
+    logo.style.height = "320px"
+    logo.style.width = "560px"
+    logo.style.top="55px";
+    logo.style.left="360px";
+  }
+  elements.forEach(element => {
+      const el = document.querySelector(element.selector); 
+      if (el) { 
+          el.style.transform = `translateY(${scrollTop * element.speed}px)`;
+      }
+  });
+});
